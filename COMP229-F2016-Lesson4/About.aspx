fdb27ae2fcdf67e970c33907313e593ca098b3da
Laash <Laash@DESKTOP-PPNPE7P>
2016-09-28 19:11:55 -0400

    Add project files.

---
+++ COMP229-F2016-Lesson4/About.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="COMP229_F2016_Lesson4.About" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="conatainer">
+        <div class="row">
+            <div class="col-md-offset-4 col-md-6">
+                <h1>About Me</h1>
+
+            </div>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ COMP229-F2016-Lesson4/Contact.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace COMP229_F2016_Lesson4
+{
+    public partial class Contact : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SendButton_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("First Name: " + FirstNameTextBox.Text);
+            Debug.WriteLine("Last Name: " + LastNameTextBox.Text);
+            Debug.WriteLine("Email: " + EmailTextBox.Text);
+            Debug.WriteLine("Contact Number: " + ContactNumberTextBox.Text);
+        }
+    }
+}
